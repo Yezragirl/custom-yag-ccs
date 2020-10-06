a9fce1d880a342c50c081d603794868660e01375
Yezragirl <54454308+Yezragirl@users.noreply.github.com>
2020-10-06 10:57:16 -0400

    folderizing

--- GO Files/Violations/18 - add violations.go
+++ GO Files/Violations/18 - add violations.go
@@ -20,8 +20,9 @@
 {{else}}
 {{sendMessage nil (joinStr "" "<@" ($args.Get 1).ID ">, " $warning " " ($args.Get 2))}}
 {{end}}
-{{if gt (toString $fine) "0"}}
-{{sendMessageNoEscape 573860427721605120 (joinStr "" (mentionRoleID 573210843840249889) ", " (getMember ($args.Get 1).ID).Nick " needs to be fined " $fine " BBS for violations of Rule " ($args.Get 0) ".")}}
+{{if or (eq (toString $fine) "0") (not $fine)}}
+{{else}}
+{{sendMessageNoEscape nil (joinStr "" (mentionRoleID 573210843840249889) ", " (getMember ($args.Get 1).ID).Nick " needs to be fined " (toString (toInt $fine)) " BBS for violations of Rule " ($args.Get 0) ".")}}
 {{end}}
 {{deleteTrigger 1}}
- 
+
---
+++ GO Files/Violations/23 - clear violations.go
@@ -0,0 +1,4 @@
+{{$args := parseArgs 2 "correct syntax is `clearv rule# who`"
+    (carg "int" "Rule Number")
+    (carg "userid" "Violator")}}
+{{dbSet ($args.Get 1) ($args.Get 0) 0}}
